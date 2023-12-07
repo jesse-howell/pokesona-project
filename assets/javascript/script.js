259115670ed612bbf66d458ff9ef828c5cc021d5
sailorshy94 <sbutle94@gmail.com>
2023-12-06 19:42:05 -0500

    edited JS file for clarity and structure, started on quiz pages section of file, edited README file and added information

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,30 +1,28 @@
 //Script
 
 
-
-
 // Pokemon Api Section Start ====================================
 
 
 function accessPokemonApi() {
-        var pokemonApiCall = "https://pokeapi.co/api/v2/pokemon?&limit=151&";
+    var pokemonApiCall = "https://pokeapi.co/api/v2/pokemon?&limit=151&";
 
-        fetch(pokemonApiCall)
-        .then(function (response){
+    fetch(pokemonApiCall)
+        .then(function (response) {
             return response.json()
         })
-            .then(function (data){
+        .then(function (data) {
 
-                var pokemonApiData = data;
-                // console.log(pokemonApiData)
+            var pokemonApiData = data;
+            // console.log(pokemonApiData)
 
 
-                //this variable will be set dynamically based off the pokesona quiz
-                var pokePick = 1;
+            //this variable will be set dynamically based off the pokesona quiz
+            var pokePick = 1;
 
-                findAPokemon(pokePick);
+            findAPokemon(pokePick);
 
-            })
+        })
 
 
 }
@@ -34,37 +32,38 @@ accessPokemonApi();
 
 
 function findAPokemon(pokePick) {
-    var pokedexNumber = "https://pokeapi.co/api/v2/pokemon/"+pokePick+"/";
+    var pokedexNumber = "https://pokeapi.co/api/v2/pokemon/" + pokePick + "/";
 
     fetch(pokedexNumber)
-    .then(function (response){
-        return response.json()
-    })
-        .then(function (data){
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
 
-            
+
             var pokemonApiData = data;
-            console.log(pokemonApiData)
+            console.log(pokemonApiData);
 
 
             var pokeName = pokemonApiData.name;
-            console.log("Pokemon Name: " + pokeName)
-            accessGiphyPokemonApi(pokeName)
-            
+            console.log("Pokemon Name: " + pokeName);
+            accessGiphyPokemonApi(pokeName);
+
 
             var pokeType = pokemonApiData.types[0].type.name;
-                console.log("Pokemon Type: " + pokeType)
+            console.log("Pokemon Type: " + pokeType);
 
 
 
             var pokeAbilitiesArray = pokemonApiData.abilities;
-                function countPokeAbilities(pokeAbilitiesArray) {
-                    for (i = 0; i <pokeAbilitiesArray.length; i++){
-                        console.log("Pokemon Abilities "+(i+1)+": " + pokeAbilitiesArray[i].ability.name)
-                    }
-                } 
 
-                countPokeAbilities(pokeAbilitiesArray)
+            function countPokeAbilities(pokeAbilitiesArray) {
+                for (i = 0; i < pokeAbilitiesArray.length; i++) {
+                    console.log("Pokemon Abilities " + (i + 1) + ": " + pokeAbilitiesArray[i].ability.name);
+                }
+            }
+
+            countPokeAbilities(pokeAbilitiesArray);
 
         })
 
@@ -76,52 +75,46 @@ function findAPokemon(pokePick) {
 
 // Giphy Api Section Start ======================================
 
-var giphyApiKey = "832va7uRpJ7h6cRIeQSDqVb72uTktwtu"
+var giphyApiKey = "832va7uRpJ7h6cRIeQSDqVb72uTktwtu";
 
 
 
 function accessGiphyPokemonApi(pokeName) {
 
-    var giphyPokemonApiCall = "https://api.giphy.com/v1/gifs/search?&q="+pokeName+"&api_key="+ giphyApiKey;
+    var giphyPokemonApiCall = "https://api.giphy.com/v1/gifs/search?&q=" + pokeName + "&api_key=" + giphyApiKey;
+
     fetch(giphyPokemonApiCall)
-    .then(function(response){
-        return response.json()
-    })
-    .then(function(data){
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
 
 
-        var pokeGifArray = data
-        console.log(pokeGifArray)
+            var pokeGifArray = data;
+            console.log(pokeGifArray);
 
-        //Dynamically calls pokemon imgs based off pokeName passed-in-variable
-        var pokeImg = data.data[0].image.original_still.url;
-        console.log("Pokemon Img Link: " + pokeImg)
+            //Dynamically calls pokemon imgs based off pokeName passed-in-variable
+            var pokeImg = data.data[0].image.original_still.url;
+            console.log("Pokemon Img Link: " + pokeImg);
 
-        //will be img in html
-        var imgLink = document.getElementById('pic')
-        imgLink.setAttribute('src', pokeImg)
+            //will be img in html
+            var imgLink = document.getElementById('pic');
+            imgLink.setAttribute('src', pokeImg);
 
-        //Dynamically calls pokemon giphs based off pokeName passed-in-variable
-        var pokeGiph = data.data[0].images.original.url;
-        console.log("Pokemon Gif Link: " + pokeGiph)
+            //Dynamically calls pokemon giphs based off pokeName passed-in-variable
+            var pokeGiph = data.data[0].images.original.url;
+            console.log("Pokemon Gif Link: " + pokeGiph);
 
-        //will be giph in html
-        var giphLink = document.getElementById('gif')
-        giphLink.setAttribute('src', pokeGiph)
+            //will be giph in html
+            var giphLink = document.getElementById('gif');
+            giphLink.setAttribute('src', pokeGiph);
 
-    })
-
-
-}
-
-
-
-
-
-
-
-
-
-
+        })
+};
 
 // Giphy Api Section End ======================================
+
+
+// Quiz Pages Section Start ===================================
+
+var pokeStartPg = document.querySelector("#start");
